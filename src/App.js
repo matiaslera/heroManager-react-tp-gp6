941903c096f822cca0f7f9e5666ce9964bc8ffa1
Matias Lera Maldonado <37850932+matiaslera@users.noreply.github.com>
2019-11-21 15:28:33 -0300

    creando primer componente

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import ButtonAppBar from './componentes/toolbar/toolbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title"> <Button variant="contained" color="primary">
+      Hola Mundo!
+    </Button></h1>      
       </header>
+
+      <ButtonAppBar ></ButtonAppBar>
     </div>
   );
 }
---
+++ src/componentes/item/items.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Table from '@material-ui/core/Table'
+
+
+export class ItemsComponent extends Component {
+
+
+
+    render() {
+
+        return (
+
+            <Table></Table>
+        )
+    }
+
+}
+
+
+export default ItemsComponent
